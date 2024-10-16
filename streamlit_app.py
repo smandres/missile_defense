0a0a2ab0e528580750e27f0dedccbd9e937eff83
smandres <120761861+smandres@users.noreply.github.com>
2024-10-16 17:06:26 +0000

    rm sections toggle

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.set_page_config(layout="wide")
 #sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
 nav = get_nav_from_toml(
-    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
-)
+    ".streamlit/pages_sections.toml")# if sections else ".streamlit/pages.toml"
+#)
 
 #st.logo("logo.png")
 
