b79ba87f2d9e783c693817e8d0bfc4cff506700c
smandres <120761861+smandres@users.noreply.github.com>
2024-10-16 16:46:35 +0000

    test pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+from st_pages import add_page_title, get_nav_from_toml
+
+st.set_page_config(layout="wide")
+
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
 )
+
+st.logo("logo.png")
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
