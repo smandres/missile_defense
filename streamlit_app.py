c4943f6bd127c42c78ac171c1f2afd53606445e2
smandres <120761861+smandres@users.noreply.github.com>
2024-10-16 17:07:21 +0000

    add back sections

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(layout="wide")
 
-#sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
 nav = get_nav_from_toml(
-    ".streamlit/pages_sections.toml")# if sections else ".streamlit/pages.toml"
-#)
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+)
 
 #st.logo("logo.png")
 
