e2d872f48b1ee3bd31a865e3280c4c0dc42b19e3
smandres <120761861+smandres@users.noreply.github.com>
2024-10-16 17:43:54 +0000

    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,18 @@ def main():
     st.title(f'☄️ Missile Defense')
 
     st.header("Help Policy")
-    st.markdown("**Authorized Resources**")
+    
     st.markdown("""
-    Any, **EXCEPT** another
-     cadet’s code (in any form) or Large Language Models (e.g., ChatGPT, Bard, etc.)
+                **Authorized Resources**: Any, **EXCEPT** another cadet’s code (in any form) or Large Language Models (e.g., ChatGPT, Bard, etc.)
     """)
+
     st.markdown("""
     Never copy another person’s work, including another cadet’s or a solution found online, and submit it as your own.
     Do not jointly create a program.
-    LLMs, such as ChatGPT, are not allowed to write code for this assignment.   
+    LLMs, such as ChatGPT, are not allowed to write code for this assignment.
+    """)
+
+    st.markdown("""
     You must document all help received from sources other than an instructor.
     DFCS will recommend a course grade of F for any cadet who egregiously violates this Help Policy or contributes to a violation by others.
     """)
