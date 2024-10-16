3a8b67cadb43408a76ce62b232e25e4541ce0a41
smandres <120761861+smandres@users.noreply.github.com>
2024-10-16 17:20:21 +0000

    reformat

---
+++ pages/spiral_one.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def main():
+    # builds the sidebar menu
+    with st.sidebar:
+        st.page_link('streamlit_app.py', label='Missile Defense', icon='☄️')
+        st.page_link('pages/spiral_one.py', label='Spiral One', icon='🌠')
+        st.page_link('pages/spiral_two.py', label='Spiral Two', icon='👩‍🚀')
+
+    st.title(f'🌠 Spiral One')
+
+    # your content
+
+
+if __name__ == '__main__':
+    main()
